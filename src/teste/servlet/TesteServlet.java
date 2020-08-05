c81d24853ead4a4c2ae0bd0ef5b9efaa765abe47
vitoremanueldev <veslima3@gmail.com>
2020-08-04 23:51:13 -0300

    Learning Java Web by playing with servlets

---
+++ src/teste/servlet/TesteServlet.java
@@ -0,0 +1,33 @@
+package teste.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//teste
+@WebServlet(urlPatterns="/teste")
+
+public class TesteServlet extends HttpServlet{
+	
+
+		@Override
+	    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		 
+			PrintWriter out = resp.getWriter();
+	        out.println("<html>");
+	        out.println("<body>");
+	        out.println("<h1>");
+	        out.println("This is a test page.");
+	        out.println("</h1>");
+	        out.println("</body>");
+	        out.println("</html>");
+
+	        System.out.println("O Servlet de teste foi chamado.");
+	    }
+
+}
+
---
+++ src/teste/servlet/registerData.java
@@ -0,0 +1,31 @@
+package teste.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class registerData
+ */
+@WebServlet("/register")
+public class registerData extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Registering new data...");
+        String dataInput = request.getParameter("data");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>Your input, " + dataInput + ", was successfully registered!</body></html>");
+    //type http://ip:port/folder/url_ending?variable=data_input
+    // http://localhost:8080/webPage/register?data=dataInput
+	}
+
+}
