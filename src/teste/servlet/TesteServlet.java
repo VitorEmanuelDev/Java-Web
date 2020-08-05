2201120e36d74a5afbc45143c848e927d319662a
vitoremanueldev <veslima3@gmail.com>
2020-08-05 02:07:29 -0300

    Using get and post methods

--- src/teste/servlet/TesteServlet.java
+++ src/teste/servlet/TesteServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class TesteServlet extends HttpServlet{
 	
+	private static final long serialVersionUID = 1L;
 
 		@Override
 	    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
--- src/teste/servlet/registerData.java
+++ src/teste/servlet/registerData.java
@@ -12,19 +12,24 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class registerData
  */
-@WebServlet("/register")
+@WebServlet(urlPatterns = "/register")
 public class registerData extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		System.out.println("Registering new data...");
+		
         String dataInput = request.getParameter("data");
+        
         PrintWriter out = response.getWriter();
+        
         out.println("<html><body>Your input, " + dataInput + ", was successfully registered!</body></html>");
-    //type: http://ip:port/folder/url_ending?variable=data_input
+    //type: http://ip:port/folder/url_pattern?variable=data_input
     //ex: http://localhost:8080/webPage/register?data=test
 	}
 
